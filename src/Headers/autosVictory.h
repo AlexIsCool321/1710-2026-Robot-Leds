1e79e1c3c344736f17c72ebbb75e73750c282830
AlexIsCool321 <alex.m.diehl1@gmail.com>
2026-02-07 14:12:15 -0600

    AutoVictory init

---
+++ src/Headers/autosVictory.h
@@ -0,0 +1,8 @@
+#ifndef AUTOVIRCTORY_H
+#define AUTOVIRCTORY_H
+
+#include <FastLED.h>
+
+void autoVictroy(CRGB* leds, int led_count);
+
+#endif
---
+++ src/Sources/autosVictory.cpp
@@ -0,0 +1,8 @@
+#include <Headers/autosVictory.h>
+#include <Headers/utils.h>
+
+
+void autoVictroy(CRGB* leds, int led_count)
+{
+	
+}
