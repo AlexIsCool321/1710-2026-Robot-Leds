3cf526f6069695a29209fbdc843b72cbd4560435
AlexIsCool321 <alex.m.diehl1@gmail.com>
2026-03-26 19:23:31 -0500

    PLEASE PLEASE ARDUION NANO WORK D:

--- src/Sources/brownOut.cpp
+++ src/Sources/brownOut.cpp
@@ -1,26 +1,22 @@
 #include <Headers/brownOut.h>
 
-#define FADE_TICKS 50
+#define FADE_TICKS 100
 
-#define COLOR_A CRGB(123, 63, 0)
-#define COLOR_B CRGB(0, 0, 0)
+#define COLOR_A CRGB(0, 255, 0)
+#define COLOR_B CRGB(0, 20, 0)
 
 void brownOutLights(CRGB* leds, uint8_t led_count)
 {
 	static uint16_t ticks = 0;
 	
 	ticks++;
-	if (ticks > FADE_TICKS)
+	if (ticks > FADE_TICKS * 2)
 	{
 		ticks = 0;
 	}
 
 	for (uint8_t i = 0; i < led_count; i++)
 	{
-		float percentage = (FADE_TICKS - ticks) / FADE_TICKS;
-
-		leds[i].r = (percentage * COLOR_A.r) + ((1-percentage) * COLOR_B.r);
-		leds[i].g = (percentage * COLOR_A.g) + ((1-percentage) * COLOR_B.g);
-		leds[i].b = (percentage * COLOR_A.b) + ((1-percentage) * COLOR_B.b);
+		leds[i] = (ticks <= FADE_TICKS) ? COLOR_A : COLOR_B;
 	}
 }
--- src/Sources/main.cpp
+++ src/Sources/main.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #include <FastLED.h>
 #include <Math.h>
 #include <SoftwareSerial.h>
@@ -8,53 +10,17 @@
 #include <Headers/autosVictory.h>
 #include <Headers/ADIndication.h>
 
-#define RX_PIN 0  // RoboRIO TX to Arduino RX
-#define TX_PIN 1  // Not needed, but required for SoftwareSerial
 #define LED1 7
-
-SoftwareSerial mySerial(RX_PIN, TX_PIN);
+#define INPUT_PIN 3
 
 #define NUM_LEDS 20
 CRGB led[NUM_LEDS];
 
-bool autosVictory	= false;
-bool BrownOut		= false;
-bool Attacking		= false;
-uint8_t FlywheelChargePercent = 0;
-
-void serialEvent()
-{
-	while (mySerial.available())
-	{
-		uint8_t receivedByte = mySerial.read();
-		if (receivedByte >= 0)
-		{
-			/*
-			 * 0	Flywheel Charge
-			 * 1	Flywheel Charge
-			 * 2	Flywheel Charge
-			 * 3	Flywheel Charge
-			 * 4	Flywheel Charge
-			 * 5	Flywheel Charge
-			 * 6	Autos Victroy
-			 * 7	Brown Out
-			 * 8	Autos Victory
-			*/
-
-			autosVictory	= (receivedByte >> 8) & 1;
-			BrownOut		= (receivedByte >> 7) & 1;
-			autosVictory	= (receivedByte >> 6) & 1;
-
-			uint8_t charge = (receivedByte << 3) >> 3; // Clears last 3 bits
-			FlywheelChargePercent = (charge / 30) * 100;
-		}
-	}
-}
-
 
 void setup()
 {
-	mySerial.begin(115200);
+	pinMode(INPUT_PIN, INPUT);
+
 	FastLED.addLeds<WS2812, LED1, RGB>(led, NUM_LEDS);
 	FastLED.clear();
 	FastLED.show();
@@ -62,9 +28,31 @@ void setup()
 
 void loop()
 {
-	if (autosVictory)	autoVictroy(led, NUM_LEDS);
-	if (brownOutLights)	brownOutLights(led, NUM_LEDS);
+	uint8_t receivedByte = digitalRead(INPUT_PIN);
+	/*
+	 * 0	Flywheel Charge
+	 * 1	Flywheel Charge
+	 * 2	Flywheel Charge
+	 * 3	Flywheel Charge
+	 * 4	Flywheel Charge
+	 * 5	Flywheel Charge
+	 * 6	Autos Victroy
+	 * 7	Brown Out
+	 * 8	Autos Victory
+	*/
+
+	bool autosVictory	= (receivedByte >> 8) & 1;
+	bool BrownOut		= (receivedByte >> 7) & 1;
+	bool Attacking		= (receivedByte >> 6) & 1;
+
+	uint8_t FlywheelChargePercent = (receivedByte << 3) >> 3; // Clears last 3 bits
+	FlywheelChargePercent = (FlywheelChargePercent / 30) * 100;
+
+
+	if (BrownOut) brownOutLights(led, NUM_LEDS);
 	ADIndication(Attacking, led, NUM_LEDS);
+
+	if (autosVictory) autoVictroy(led, NUM_LEDS);
 	flywheelChargeLights(FlywheelChargePercent, led, NUM_LEDS);
 
 	FastLED.show();
